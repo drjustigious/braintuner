5148911dbecd249d90a7312055a113efccdfa6a2
Jussi Tikkanen <drjustigious@gmail.com>
2018-10-27 23:35:13 +0300

    Added a global configuration file.

---
+++ BrainConfig.hpp
@@ -0,0 +1,13 @@
+#ifndef BRAIN_CONFIG_HPP
+#define BRAIN_CONFIG_HPP
+
+namespace BrainConfig {
+
+const unsigned int FRAME_RATE = 60;
+const unsigned int INITIAL_WINDOW_WIDTH = 1024;
+const unsigned int INITIAL_WINDOW_HEIGHT = 768;
+
+} // namespace BrainConfig
+
+
+#endif /* BRAIN_CONFIG_HPP */
